db8955749d4104cd35a2f6c334f45b8050704ad9
Pao-Duran <135394780+Paola-cecilia-Duran@users.noreply.github.com>
2023-12-02 22:31:34 -0400

    login y mejoramiento del template

--- Sis457Restaurante/WebRestaurante/obj/Debug/net7.0/WebRestaurante.AssemblyInfo.cs
+++ Sis457Restaurante/WebRestaurante/obj/Debug/net7.0/WebRestaurante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebRestaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3183faed16ff92300fec84606d845dccb9c6f04c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebRestaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebRestaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
